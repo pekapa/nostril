5a70495cb18d42f013bcf6211fe10d8245e87687
Pedro Paiva <cpaiva.pedro@gmail.com>
2016-06-27 18:08:10 -0300

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='nostril',
+    packages=['nostril'],
+    version='0.1',
+    description=('Simple nose wrapper to easily test packages '
+                 'from within python.'),
+    author='Pedro Paiva',
+    author_email='p3k4p4@gmail.com',
+    url='https://github.com/pekapa/nostril',
+    download_url='https://github.com/pekapa/nostril/tarball/0.1',
+    keywords=['testing', 'nose'],
+    classifiers=[],
+)
